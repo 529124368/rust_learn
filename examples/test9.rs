567db402143f0ce0d606a42d5d71d60e9c529f95
529124368 <529124368@qq.com>
2022-07-19 20:02:52 +0900

    update

---
+++ examples/test9.rs
@@ -0,0 +1,49 @@
+fn main() {
+    let mut p = Point::new();
+    p.setX(123);
+    p.setY(12);
+    println!("{:#?}", p);
+    println!("{}", p.getX());
+    println!("{}", p.getY());
+    let a = getob(&p);
+    println!("{}", a);
+    println!("{:?}", p);
+    setob(&mut p, 11);
+    println!("{:?}", p);
+}
+trait Flg: std::default::Default + Copy {}
+impl<T> Flg for T where T: std::default::Default + Copy {}
+
+fn getob<T: Flg>(p: &Point<T>) -> &T {
+    p.getX()
+}
+
+fn setob<T: Flg>(p: &mut Point<T>, a: T) {
+    p.setX(a);
+}
+
+#[derive(Default, Debug)]
+struct Point<T> {
+    x: T,
+    y: T,
+}
+impl<T: Flg> Point<T> {
+    fn new() -> Self {
+        Point::default()
+    }
+
+    fn getX(&self) -> &T {
+        &self.x
+    }
+
+    fn getY(&self) -> &T {
+        &self.y
+    }
+    fn setX(&mut self, x: T) {
+        self.x = x
+    }
+
+    fn setY(&mut self, y: T) {
+        self.y = y
+    }
+}
