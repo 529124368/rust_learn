a27f4b00be2fae4c8cf6a7df3c27238768e444e7
529124368 <529124368@qq.com>
2022-08-06 20:42:09 +0900

    派生宏demo练习强化

--- examples/test13.rs
+++ examples/test13.rs
@@ -7,10 +7,15 @@
 
 use zimu_macro::zimuMacro;
 
-#[derive(zimuMacro)]
-struct test;
+#[derive(zimuMacro, Default)]
+struct Test {
+    name: String,
+    sex: u8,
+}
 
 fn main() {
-    test::hello_world();
+    let s = Test::new();
+    println!("{}", s.get_name());
+    println!("{}", s.get_sex());
     // println!("{}", sqr!(1 + 1));
 }
--- zimu_macro/src/lib.rs
+++ zimu_macro/src/lib.rs
@@ -1,14 +1,18 @@
 extern crate proc_macro;
 use crate::proc_macro::TokenStream;
 use quote::quote;
-use syn;
+use syn::{self, ext::IdentExt, parse_macro_input, DeriveInput};
 
 fn impl_zimu_macro(ast: &syn::DeriveInput) -> TokenStream {
     let name = &ast.ident;
+
     let gen = quote! (
         impl  #name {
             fn hello_world() {
-                println!("我的第一个派生宏")
+                println!("我的第一个派生宏");
+            }
+            fn test(&self) {
+                println!("我的第一个派生宏");
             }
         }
     );
@@ -17,6 +21,39 @@ fn impl_zimu_macro(ast: &syn::DeriveInput) -> TokenStream {
 
 #[proc_macro_derive(zimuMacro)]
 pub fn zimu_macro_derive(input: TokenStream) -> TokenStream {
-    let ast = syn::parse(input).unwrap();
-    impl_zimu_macro(&ast)
+    // let ast = syn::parse(input).unwrap();
+    let ast = parse_macro_input!(input as DeriveInput);
+
+    let d = match &ast.data {
+        syn::Data::Struct(s) => &s.fields,
+        _ => todo!(),
+    };
+    let mut boxs = Vec::new();
+    for i in d.iter() {
+        let name = i.ident.as_ref().unwrap();
+        let ty = &i.ty;
+        let method_name: proc_macro2::TokenStream =
+            format!("get_{}", name.unraw().to_string()).parse().unwrap();
+        boxs.push(quote! (
+            fn #method_name(&self) -> #ty {
+                self.#name.clone()
+            }
+        ));
+    }
+
+    let name = &ast.ident;
+
+    let gen = quote! (
+        impl  #name {
+            fn new() ->Self {
+                #name::default()
+            }
+            fn hello_world() {
+                println!("我的第一个派生宏");
+            }
+            #(#boxs)*
+        }
+    );
+    gen.into()
+    //impl_zimu_macro(&ast)
 }
