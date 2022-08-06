c4b8b24b2f94ed22547912e6faf11e6561e9d513
529124368 <529124368@qq.com>
2022-08-06 23:45:58 +0900

    update

--- zimu_macro/src/lib.rs
+++ zimu_macro/src/lib.rs
@@ -3,22 +3,6 @@ use crate::proc_macro::TokenStream;
 use quote::quote;
 use syn::{self, ext::IdentExt, parse_macro_input, DeriveInput};
 
-fn impl_zimu_macro(ast: &syn::DeriveInput) -> TokenStream {
-    let name = &ast.ident;
-
-    let gen = quote! (
-        impl  #name {
-            fn hello_world() {
-                println!("我的第一个派生宏");
-            }
-            fn test(&self) {
-                println!("我的第一个派生宏");
-            }
-        }
-    );
-    gen.into()
-}
-
 #[proc_macro_derive(zimuMacro)]
 pub fn zimu_macro_derive(input: TokenStream) -> TokenStream {
     // let ast = syn::parse(input).unwrap();
@@ -55,5 +39,4 @@ pub fn zimu_macro_derive(input: TokenStream) -> TokenStream {
         }
     );
     gen.into()
-    //impl_zimu_macro(&ast)
 }
