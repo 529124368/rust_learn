6d63e4456c3afe81bb652998ce4e1b43e7aa6b8c
529124368 <529124368@qq.com>
2022-08-07 00:28:52 +0900

    派生宏demo练习强化

--- zimu_macro/src/lib.rs
+++ zimu_macro/src/lib.rs
@@ -1,11 +1,10 @@
 extern crate proc_macro;
 use crate::proc_macro::TokenStream;
 use quote::quote;
-use syn::{self, ext::IdentExt, parse_macro_input, DeriveInput};
+use syn::{self, ext::IdentExt, parse_macro_input, DeriveInput, TypeParen};
 
 #[proc_macro_derive(zimuMacro)]
 pub fn zimu_macro_derive(input: TokenStream) -> TokenStream {
-    // let ast = syn::parse(input).unwrap();
     let ast = parse_macro_input!(input as DeriveInput);
 
     let d = match &ast.data {
@@ -16,11 +15,26 @@ pub fn zimu_macro_derive(input: TokenStream) -> TokenStream {
     for i in d.iter() {
         let name = i.ident.as_ref().unwrap();
         let ty = &i.ty;
+        //println!("@@{:#?}", ty);
+        let ss = match ty {
+            syn::Type::Path(a) => a.path.leading_colon,
+            _ => todo!(),
+        };
+        println!("@@{:#?}", ss);
+
+        //
         let method_name: proc_macro2::TokenStream =
             format!("get_{}", name.unraw().to_string()).parse().unwrap();
+        let method_name2: proc_macro2::TokenStream =
+            format!("set_{}", name.unraw().to_string()).parse().unwrap();
         boxs.push(quote! (
+            //get
             fn #method_name(&self) -> #ty {
                 self.#name.clone()
+            }
+            //set
+            fn #method_name2(&mut self,v:#ty){
+                self.#name =v
             }
         ));
     }
