2323e4244dd000ea9f9addeea2626e6100dd9573
529124368 <529124368@qq.com>
2022-08-14 19:24:04 +0900

    update

--- examples/test14.rs
+++ examples/test14.rs
@@ -1,3 +1,13 @@
 fn main() {
-    
+    let mut s = "$%7fsdfs".to_string();
+    {
+        s = "a".to_string();
+    }
+    {
+        s = "s".to_string();
+    }
+    {
+        s = "d".to_string();
+    }
+    println!("{s}");
 }
